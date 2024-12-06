e7af39b3f47241a9cd76157e2a9663dcfffe60f2
allen-w-camp <allenwcamp@gmail.com>
2024-12-06 09:10:12 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import "./App.css";
 import { useState } from "react";
-import Authenticate from ".components/authenticate";
-import SignUpForm from ".components/signupform";
+import "./App.css";
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
 
 function App() {
   const [token, setToken] = useState(null);
 
   return (
     <>
-            
       <SignUpForm token={token} setToken={setToken} />
-            
       <Authenticate token={token} setToken={setToken} />
-          
     </>
   );
 }
+
+export default App;
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,59 +1,68 @@
 import { useState } from "react";
 
-export default function SignUpForm() {
+export default function SignUpForm({ setToken }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
   const [error, setError] = useState(null);
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-    console.log("Hello 👋");
+  async function handleSubmit(e) {
+    e.preventDefault();
     try {
-        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup",
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
         {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                  username: {username},
-                  password: {password},
-                },
-              }
-            );
-const result = await response.json();
-console.log(result);
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        }
+      );
+      const result = await response.json();
+      console.log("Signup Result: ", result);
+      setToken(result.token);
+      setSuccessMessage(result.message);
+      setUsername("");
+      setPassword("");
     } catch (error) {
       setError(error.message);
     }
   }
 
   return (
-    <>
-            <h2>Sign Up</h2>
-            {error && <p>{error}</p>}
-            
+    <div>
+      <h2>Signup</h2>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
-                
         <label>
-                    Username:           
+          Username:{" "}
           <input
+            required
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-                  
         </label>
-                
+        <br />
         <label>
-                    Password:           
+          Password:{" "}
           <input
             type="password"
+            required
+            minLength={8}
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />      
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
         </label>
-                <button>Submit</button>
-              
+        <br />
+        <button>Submit</button>
       </form>
-          
-    </>
+    </div>
   );
 }
